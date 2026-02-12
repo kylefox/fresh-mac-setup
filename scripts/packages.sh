9ef3e1da50ba857424e2df2a45c9dae9132604f8
Kyle Fox <email@kylefox.com>
2026-02-11 22:02:14 -0700

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,4 +2,15 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-brew bundle --file="$SCRIPT_DIR/../Brewfile"
+
+echo "Updating Homebrew..."
+brew update
+
+echo "Installing packages..."
+brew bundle --verbose --file="$SCRIPT_DIR/../Brewfile"
+
+echo "Cleaning up..."
+brew cleanup
+
+echo "Running brew doctor..."
+brew doctor
